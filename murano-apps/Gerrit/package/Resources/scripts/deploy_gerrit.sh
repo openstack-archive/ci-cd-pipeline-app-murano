73544dd77cfde20123bf26b3b02469c7a7f1a162
Mykyta Karpin <mkarpin@mirantis.com>
2016-03-25 15:57:37 +0200

    [Gerrit] Implement automation of project creation
    Change-Id: I980a0e20384205935edcb84a7bcdf0538a883792

--- murano-apps/Gerrit/package/Resources/scripts/deploy_gerrit.sh
+++ murano-apps/Gerrit/package/Resources/scripts/deploy_gerrit.sh
@@ -2,6 +2,9 @@
 
 logger Deploy Gerrit
 
+#TODO add this template to system-config
+cp gerrit_gitconfig.erb /etc/puppet/modules/openstack_project/templates/
+
 logger Generate ssl certificates
 /bin/bash ./gen_ssl_cert.sh
 
@@ -13,5 +16,8 @@ puppet apply database.pp
 
 logger Gerrit puppet
 puppet apply site.pp
+
+logger Projects puppet
+puppet apply create_projects.pp
 
 exit
--- murano-apps/Gerrit/package/Resources/scripts/site.pp
+++ murano-apps/Gerrit/package/Resources/scripts/site.pp
@@ -49,7 +49,7 @@ node default {
     acls_dir                            => hiera('gerrit_acls_dir'),
     notify_impact_file                  => hiera('gerrit_notify_impact_file'),
     projects_file                       => hiera('jeepyb_project_file'),
-    projects_config                     => 'openstack_project/review-dev.projects.ini.erb',
+    projects_config                     => 'openstack_project/review-paas.projects.ini.erb',
     github_username                     => 'openstack-gerrit',
     github_oauth_token                  => hiera('gerrit_github_token'),
     github_project_username             => hiera('github_project_username', 'username'),
