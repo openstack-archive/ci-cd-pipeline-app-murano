c8b47a5de04888193a6bafc90e2087d97f5c18e8
Alexey Khivin <akhivin@mirantis.com>
2016-03-25 14:12:42 +0300

    [SystemConfig] Change project-config repo url to the inner source
    Change-Id: Id461c7c1af7b2b85083a96d1e9fda86a0cb0e901

--- murano-apps/SystemConfig/package/Resources/scripts/deploy.sh
+++ murano-apps/SystemConfig/package/Resources/scripts/deploy.sh
@@ -12,5 +12,5 @@ cd system-config
 cp -r modules/ /etc/puppet/
 
 
-git clone https://github.com/mirademo/project-config.git
+git clone https://review.fuel-infra.org/open-paas/project-config
 cp -r project-config/ /etc/
