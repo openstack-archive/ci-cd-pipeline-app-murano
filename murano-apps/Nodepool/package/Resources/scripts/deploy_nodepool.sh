6d351b90675df7ae40eb04d52a1003703550d5b7
Nikolay Mahotkin <nmakhotkin@mirantis.com>
2016-04-21 15:15:42 +0300

    Fixing Nodepool app
     * Adding credentials configuration to Jenkins app
     * Fixed nodepool configuration
     * Added retrieving API token from Jenkins
     * Providing parameters for configuration of OpenStack cloud
     * Fixed providing public and private keys to nodepool nodes
    
    Change-Id: I5c8be834f33870b9733df0041e96ffe94a9618b4

--- murano-apps/Nodepool/package/Resources/scripts/deploy_nodepool.sh
+++ murano-apps/Nodepool/package/Resources/scripts/deploy_nodepool.sh
@@ -2,11 +2,9 @@
 
 logger Deploying Nodepool...
 
-/bin/bash ./gen_rsa_key.sh
-
-mkdir /etc/puppet/modules/nodepool_configure
-mkdir /etc/puppet/modules/nodepool_configure/manifests
-mkdir /etc/puppet/modules/nodepool_configure/templates
+# No error if already exists.
+mkdir -p /etc/puppet/modules/nodepool_configure/manifests
+mkdir -p /etc/puppet/modules/nodepool_configure/templates
 
 cp nodepool_configure/manifests/init.pp /etc/puppet/modules/nodepool_configure/manifests/
 cp nodepool_configure/templates/nodepool.yaml.erb /etc/puppet/modules/nodepool_configure/templates/
