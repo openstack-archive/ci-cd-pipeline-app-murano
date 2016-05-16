de9f9dfbc281896c1a61e870866ac1cadfa151d0
Alexey Khivin <akhivin@mirantis.com>
2016-05-17 02:13:18 +0300

    [Puppet] Puppet module refactored
      * setHieraValue is deprecated now.   putHieraData introduced instead.
         Since now  it is possible to pass a dictionary object  instead of one  key/value pair
      * shell files renamed to lowcase
      * yaml files processed by python and PyYAML instead of puppet module
      * 'reidmv/yamlfile' puppet module removed
    
    Change-Id: I1b897cefb6dc9edcb860792390c2de59920e242a

--- murano-apps/Puppet/package/Resources/scripts/setYamlValue.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-path=$1
-key=$2
-value=$3
-
-logger Put value to $path $key=$value
-
-puppet apply --execute "yaml_setting { 'example': target=>'$path', key=>'$key', value=>'$value', }"
