ccdc27bf2bc98ac20fbe7b6b7d925aef6cee522e
Nikolay Mahotkin <nmakhotkin@mirantis.com>
2016-05-20 11:57:43 +0000

    Revert "[Gerrit] Switch to local project config"
    This reverts commit c0b9861333b0ace7442e521d6fac9137043fb3d9.
    
    Change-Id: If3fed821f65823e3ab590ac2bea5aa5fee0027a9

--- murano-apps/Gerrit/package/Resources/scripts/deploy_gerrit.sh
+++ murano-apps/Gerrit/package/Resources/scripts/deploy_gerrit.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-CONF_DIR="/etc/ci-cd"
-EXECUTABLES_DIR="/usr/local/bin"
-
 logger Install dev packages
 apt-get update && apt-get install -y build-essential libssl-dev libffi-dev python-dev
 
@@ -21,12 +18,6 @@ logger Database puppet
 puppet apply database.pp
 
 logger Gerrit puppet
-# store to add cron task later
-mkdir -p ${CONF_DIR}
-cp site.pp ${CONF_DIR}/gerrit.pp
-cp create_projects_periodic.sh ${EXECUTABLES_DIR}/create_projects_periodic.sh
-chmod +x ${EXECUTABLES_DIR}/create_projects_periodic.sh
-
 puppet apply site.pp
 
 logger Projects puppet
