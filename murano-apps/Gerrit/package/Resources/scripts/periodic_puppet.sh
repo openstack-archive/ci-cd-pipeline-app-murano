9b84a5edd3d19192d6399c6d621d614336368af0
Nikolay Mahotkin <nmakhotkin@mirantis.com>
2016-05-25 11:31:38 +0300

    Fixing applying ACLs for new projects
     * The issue was in incorrect calling the script
       'manage-projects': It should be invoked from
       user 'gerrit2' sharing user env.
    
    Change-Id: Ib987957e0e1a7f0c4951883ee64175b21293bdad

--- murano-apps/Gerrit/package/Resources/scripts/periodic_puppet.sh
+++ murano-apps/Gerrit/package/Resources/scripts/periodic_puppet.sh
@@ -12,4 +12,4 @@ GIT_SSL_NO_VERIFY=true /usr/bin/puppet apply ${CONF_DIR}/gerrit.pp
 # Log rotation is turned on in create_projects.pp
 #
 # TODO: Should be replaced by git hook
-/usr/local/bin/manage-projects -v -d -l /var/log/manage_projects.log
+su - gerrit2 -c "/usr/local/bin/manage-projects -v -d -l /var/log/manage_projects.log"
