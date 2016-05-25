38ba7778e52aab6b4e1638d1e16aacca19dfc968
alexz <azvyagintsev@mirantis.com>
2016-05-25 17:23:22 +0300

    add temporary test-deployment script
    Change-Id: I6b3f9369e64242505bc9f583a6c732d46ea79cca

---
+++ tools/jenkins/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+# Requirments:
+# apt-get install python-venv python-pip build-essential libssl-dev libffi-dev -y
+
+# Variables:
+VENV_PATH=${VENV_PATH:-.venv}
+VENV_CLEAN=${VENV_CLEAN:-false}
+TEST_NAME=${TEST_NAME:-none}
+
+function prepare_venv() {
+    echo 'LOG: Creating python venv for murano-client'
+    rm -rf ${VENV_PATH}
+    mkdir -p ${VENV_PATH}
+    virtualenv --system-site-packages  ${VENV_PATH}
+    source ${VENV_PATH}/bin/activate
+    #TODO install from requirments.txt ?
+    pip install python-muranoclient python-heatclient
+    deactivate
+}
+
+# Body
+if [[ ("${VENV_CLEAN}" == true) || (! -f "${VENV_PATH}/bin/activate") ]]; then
+    prepare_venv
+fi
+
+if [[ "${TEST_NAME}" != "none" ]] ; then
+    source ${VENV_PATH}/bin/activate
+    echo "LOG: Attempt to run test=${TEST_NAME}"
+    ./utils/jenkins/${TEST_NAME}
+    deactivate
+fi
+
