02a7784245456ae819c62d866de6e83c7b65c971
alexz <azvyagintsev@mirantis.com>
2016-05-25 17:23:22 +0300

    wip
    Change-Id: I6b3f9369e64242505bc9f583a6c732d46ea79cca

---
+++ utils/jenkins/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Requirments:
+# apt-get install python-venv python-pip build-essential libssl-dev libffi-dev -y
+
+# Variables:
+VENV_PATH=${VENV_PATH:-.venv}
+VENV_CLEAN=${VENV_PATH:-false}
+TEST_NAME=${TEST_NAME:-none}
+
+function prepare_venv() {
+    echo 'LOG: Creating python venv for murano-client'
+    mkdir -p ${VENV_PATH}
+    virtualenv --system-site-packages  ${VENV_PATH}
+    source ${VENV_PATH}/bin/activate
+    #TODO install from requirments.txt ?
+    pip install python-muranoclient
+}
+
+# Body
+if [[ ("${VENV_CLEAN}" == true) || (! -f "${VENV_PATH}/bin/activate") ]]; then
+    prepare_venv
+fi
+
+source ${VENV_PATH}/bin/activate
+if [[ "${TEST_NAME}" != "none" ]] ; then
+    echo "LOG: Attempt to run test=${TEST_NAME}"
+    ./utils/jenkins/${TEST_NAME}
+fi
+
+
+deactivate
