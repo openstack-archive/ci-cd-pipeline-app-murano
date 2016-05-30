689c2096565b2bbd4fc85debd88347cc794e5d11
asilenkov <asilenkov@mirantis.com>
2016-05-30 09:08:30 +0300

    CloudBees Docker Build and Publish plugin added
      * docker-build-publish plugin allows to build Docker
        images from artifacts and publish to Registry.
        Registry could be private or public.
    
    Change-Id: I5161b32149649d6963694e99b88f9e18a07d068d

---
+++ murano-apps/Jenkins/package/Resources/scripts/docker_plugin.pp
@@ -0,0 +1,28 @@
+node default {
+  service { 'jenkins':
+    ensure => running,
+    enable => true,
+  }
+
+  jenkins::plugin { 'authentication-tokens': 
+    notify  => Service['jenkins'],
+    require => Jenkins::Plugin['credentials']
+  }
+
+  jenkins::plugin { 'docker-commons':
+    notify  => Service['jenkins'],
+    require => [
+      Jenkins::Plugin['authentication-tokens'],
+      Jenkins::Plugin['icon-shim'],
+      Jenkins::Plugin['credentials']
+    ]
+  }
+
+  jenkins::plugin { 'docker-build-publish':
+    notify  => Service['jenkins'],
+    require => [
+      Jenkins::Plugin['token-macro'],
+      Jenkins::Plugin['docker-commons']
+    ]
+  }
+}
