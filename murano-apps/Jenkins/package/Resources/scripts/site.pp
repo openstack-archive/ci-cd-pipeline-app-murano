8a584b3ebe4efca1435c7dad85823fe323aec811
Anastasia Kuznetsova <akuznetsova@mirantis.com>
2016-06-02 19:15:07 +0300

    Fix automated job creation
    - Splited installation of Jenkins anf JJB
    - Changed JJB config
    - Added switching to local Gerrit config
    
    Change-Id: Iec04050b1ac3674329c9f0da60d852775d75fbbd

--- murano-apps/Jenkins/package/Resources/scripts/site.pp
+++ murano-apps/Jenkins/package/Resources/scripts/site.pp
@@ -1,16 +1,13 @@
 node default {
 
-  if ! defined(Class['project_config']) {
-    class { 'project_config':
-      url  => hiera('project_config_repo'),
-    }
-  }
-
-  class { 'openstack_project::jenkins':
+  class { 'openstackci::jenkins_master':
+    logo                    => 'openstack.png',
+    serveradmin             => 'webmaster@${::fqdn}',
     jenkins_password        => '',
     jenkins_ssh_private_key => hiera('jenkins_ssh_private_key_contents'),
+    jenkins_ssh_public_key  => hiera('jenkins_ssh_pubkey_contents'),
     ssl_cert_file           => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
     ssl_key_file            => '/etc/ssl/private/ssl-cert-snakeoil.key',
-    ssl_chain_file          => '',
+    manage_jenkins_jobs     => false,
   }
 }
