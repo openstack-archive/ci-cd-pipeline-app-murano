8fe93d9eb0490cdead5c9cf558199eafdea6f183
Anastasia Kuznetsova <akuznetsova@mirantis.com>
2016-06-02 19:15:07 +0300

    Fix automated job creation
    - Splited installation of Jenkins anf JJB
    - Changed JJB config
    - Added switching to local Gerrit config
    
    Change-Id: Iec04050b1ac3674329c9f0da60d852775d75fbbd

---
+++ murano-apps/Jenkins/package/Resources/scripts/update_jobs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /etc/project-config
+
+# Check whether we have local Gerrit installed or not.
+# If yes, then 'git pull' should be executed under 'jenkins' user
+
+GERRIT_URL=`hiera -c /etc/puppet/hiera.yaml gerrit_host`
+
+if ! [ $GERRIT_URL="nil" ]; then
+  su jenkins -c "git pull"
+else
+  git pull
+fi
+
+cp -R /etc/project-config/jenkins/jobs/* /etc/jenkins_jobs/config
+/usr/local/bin/jenkins-jobs update --delete-old /etc/jenkins_jobs/config
