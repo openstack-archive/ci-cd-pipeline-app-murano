27220928e3f206dbfad938524705a3f9c7ce6c77
Sergey Kraynev <skraynev@mirantis.com>
2016-06-10 15:22:59 +0300

    Remove unused namespaces
    Change-Id: Ica3174730276c6918aceef85aac1f968bf77ed04

---
+++ unittests/test_namespaces.py
@@ -0,0 +1,78 @@
+import os
+import re
+import yaml
+
+
+class TestNamespaces():
+    def get_list_of_classes(self):
+        # TODO: should be fixed future with some common approach for all tests
+        root_dir = os.path.dirname(os.path.abspath(__file__)).rsplit('/', 1)[0]
+        apps_dir = os.path.join(root_dir, 'murano-apps')
+        class_names = []
+
+        for path, dirs, files in os.walk(apps_dir):
+            if path.endswith('Classes'):
+                names = [os.path.join(path, f)
+                         for f in files if f.endswith('.yaml')]
+                class_names.extend(names)
+        return class_names
+
+    def get_namespaces(self, cls_name):
+        # workaround for PyYAML bug: http://pyyaml.org/ticket/221
+        ###############################
+        class YaqlYamlLoader(yaml.Loader):
+                pass
+
+        def yaql_constructor(loader, node):
+            return
+
+        YaqlYamlLoader.add_constructor(u'!yaql', yaql_constructor)
+        ###############################
+
+        # parse file
+        parsed_data = ''
+        with open(cls_name) as f:
+            parsed_data = yaml.load(f, YaqlYamlLoader)
+
+        n_spaces = parsed_data.get('Namespaces')
+        if n_spaces is None:
+            msg = 'File "%s" does not content "Namespaces" section' % cls_name
+            raise ValueError(msg)
+        # get only names of namespaces
+        names = n_spaces.keys()
+        # remove main namespace from the list
+        names.remove('=')
+        return names
+
+    def check_name(self, namespace, cls_name, error_list):
+        # read file
+        data = ''
+        with open(cls_name) as f:
+            data = f.read()
+
+        regexp_str = '%s:[A-Z]+' % namespace
+        regexp = re.compile(regexp_str)
+        if len(regexp.findall(data)) == 0:
+            msg = ('Namespace "%s" is not used in the "%s" and should '
+                   'be removed from list of Namespaces' % (namespace, cls_name))
+            error_list.append(msg)
+
+    def test_namespaces(self):
+        error_list = []
+        for cls_name in self.get_list_of_classes():
+            for ns in self.get_namespaces(cls_name):
+                self.check_name(ns, cls_name, error_list)
+
+        if error_list:
+            error_string = "\n".join(error_list)
+            msg = "Test detects follow list of errors: \n%s" % error_string
+            raise ValueError(msg)
+
+
+def main():
+    test = TestNamespaces()
+    test.test_namespaces()
+
+# to run test just execute command:
+# python unittests/test_namespaces.py
+main()
