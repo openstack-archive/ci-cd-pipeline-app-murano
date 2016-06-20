6fa40e3a862dce53acdc675a1ad49c78fa839654
Nikolay Mahotkin <nmakhotkin@mirantis.com>
2016-06-20 13:42:52 +0300

    Integration Zuul and Nodepool into CI/CD App
     * Using utility application ZuulNodepoolHelper
    
    Change-Id: I4b0e093c0583b56a1b896d4304b71bcda42c8f0c

--- tools/prepare_packages.sh
+++ tools/prepare_packages.sh
@@ -13,7 +13,7 @@ function check_dir () {
     fi
 }
 
-default_packages="Puppet SystemConfig CiCdUtils OpenLDAP Gerrit Jenkins Zuul Nodepool CiCdEnvironment"
+default_packages="Puppet SystemConfig CiCdUtils OpenLDAP Gerrit Jenkins Zuul Nodepool ZuulNodepoolHelper CiCdEnvironment"
 source_dir="murano-apps"
 destination_dir="."
 refresh_existing_packages=false
