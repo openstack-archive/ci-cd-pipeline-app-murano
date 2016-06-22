2add49b1d246ef8bf00d0993dc1519110a6c99a0
Alexey Khivin <akhivin@gmail.com>
2016-06-23 00:48:32 +0300

    [Puppet] PuppetInstance.applyManifest added
    You can now apply Puppet manifest from MuranoPL directly
    
    Change-Id: I38c5cd4c2f8af194d42931ef2d70f5d8b158cac1

---
+++ murano-apps/Puppet/package/Resources/scripts/apply_manifest.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set +e
+
+puppet apply --detailed-exitcodes --color=false "site.pp"
+
+PUPPET_RETURN=$?
+if [ "${PUPPET_RETURN}" -eq 4 ] || [ "${PUPPET_RETURN}" -eq 6 ] ; then
+    exit ${PUPPET_RETURN}
+fi
+
+set -e
