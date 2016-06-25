6082003d5ab087c7f62f8e3f42f6edf81cd739b7
Alexey Khivin <akhivin@gmail.com>
2016-06-25 18:26:07 +0300

    !WIP [Puppet] Class for building puppet cluster
    Change-Id: Idd1ef2dabf6146d62c9e9bee1831127c3c967b7e

--- murano-apps/Puppet/package/Resources/scripts/core/apply_manifest.sh
+++ murano-apps/Puppet/package/Resources/scripts/core/apply_manifest.sh
@@ -2,7 +2,7 @@
 
 set +e
 
-puppet apply --detailed-exitcodes --color=false "site.pp"
+puppet apply --detailed-exitcodes --color=false "../site.pp"
 
 PUPPET_RETURN=$?
 if [ "${PUPPET_RETURN}" -eq 4 ] || [ "${PUPPET_RETURN}" -eq 6 ] ; then
