30b5a9585c1fa16b24cd4dbc12f5b583248366eb
Alexey Khivin <akhivin@gmail.com>
2016-06-25 18:26:07 +0300

    !WIP [Puppet] Class for building puppet cluster
    Change-Id: Idd1ef2dabf6146d62c9e9bee1831127c3c967b7e

---
+++ murano-apps/Puppet/package/Resources/scripts/server/puppet_server.pp
@@ -0,0 +1,28 @@
+node default{
+  package { 'ntp':
+    ensure => 'installed',
+  }
+
+  class { '::puppet':
+    server                => true,
+    server_foreman        => false,
+    server_reports        => 'store',
+    server_http           => true,
+    server_http_port      => 8130, # default: 8139
+    server_http_allow     => [],
+    # server_http_allow    => ['10.20.30.1', 'puppetbalancer.my.corp'],
+    server_external_nodes => '',
+    server_git_repo       => true,
+    puppetmaster =>  'puppet',
+    certname =>  'mastercert',
+    server_puppetserver_version => '2.4.99'
+    # server_puppetserver_version
+  }
+
+  file { '/tmp/puppet-server':
+    ensure  => 'present',
+    replace => 'no', # this is the important property
+    content => "From Puppet\n",
+    mode    => '0644',
+  }
+}
