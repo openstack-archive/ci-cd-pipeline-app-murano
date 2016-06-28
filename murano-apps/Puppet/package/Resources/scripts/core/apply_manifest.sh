0a098669b1cc3e41fcd6947e6f67f1bf91cc5fbc
Alexey Khivin <akhivin@gmail.com>
2016-06-29 02:45:36 +0300

    WIP [Puppet] Demoapp
    Change-Id: I0becebfb46fd046f4d93ec14f1daf1aa26065023

--- murano-apps/Puppet/package/Resources/scripts/core/apply_manifest.sh
+++ murano-apps/Puppet/package/Resources/scripts/core/apply_manifest.sh
@@ -1,8 +1,3 @@
-#!/usr/bin/env bash
-
-# As described in https://bugs.launchpad.net/murano/+bug/1598232
-# murano-agent set the working directory depending on where the script
-# is located. Check and set working directory to the root of the
 # temporary folder which created by murano for the current execution plan
 # This code must work even if LP1598232 will be fixed
 
