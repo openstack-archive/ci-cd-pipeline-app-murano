48125f9cdbad6712bde4ab954a3083710efcbb20
Alexey Khivin <akhivin@gmail.com>
2016-07-04 14:20:06 +0300

    [Puppet] applyManifest fixed
    As described in LP-1598232
    murano-agent set the working directory depending on where the script is
    located. Check and set working directory to the root of the
    temporary folder which created by murano for the current execution plan
    This code must work even if LP-1598232 will be fixed
    
    Change-Id: Id9a29a9d8ea49b56ae8d3457c0308121e390cc62

--- murano-apps/Puppet/package/Resources/scripts/core/apply_manifest.sh
+++ murano-apps/Puppet/package/Resources/scripts/core/apply_manifest.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# As described in https://bugs.launchpad.net/murano/+bug/1598232
+# murano-agent set the working directory depending on where the script
+# is located. Check and set working directory to the root of the
+# temporary folder which created by murano for the current execution plan
+# This code must work even if LP1598232 will be fixed
+
+if [ -d ../core ]
+then
+    cd ..
+fi
+
 set +e
 
 puppet apply --detailed-exitcodes --color=false "site.pp"
