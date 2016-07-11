5ef8122c7361c8b93e315b97325251d44dec20ea
Sergey Kraynev <skraynev@mirantis.com>
2016-07-11 19:13:15 +0300

    Move system-config url to properties
    Make system-config url available for configuring by setting
    it out of the class. Also add property for choosing which branch should
    be used stable or master. By default it will be "master" branch with url
    pointing on fuel-infra repository.
    
    Also add UI form for SystemConfig package, which will be used later by
    UI forms of Applications.
    
    Change-Id: If8c11e6ad159980fe1bac8eded095de57023a356

--- murano-apps/SystemConfig/package/Resources/scripts/deploy.sh
+++ murano-apps/SystemConfig/package/Resources/scripts/deploy.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+URL="$1"
+BRANCH="$2"
+PATCH_ID="$3"
+
 logger Cloning openstack-ci system-config
 
-git clone https://review.fuel-infra.org/open-paas/system-config
+gclonecd() {
+    git clone "$1" && cd "$(basename "$1")"
+}
+
+if [ -n $PATCH_ID ] ; then
+   gclonecd $URL
+   patch_ref=`git ls-remote | grep $PATCH_ID | tail -1 | awk '{print $2}'`
+   git fetch $URL $patch_ref && git checkout "FETCH_HEAD"
+elif [-n $BRANCH ] ; then
+  git clone $URL --branch $BRANCH
+else
+  git clone $URL
+fi
 
 logger Installing openstack-ci system-config
 
