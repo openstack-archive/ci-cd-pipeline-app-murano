ddebc87d239d71ed4b786f40adf31ada1038b5fb
Sergey Kraynev <skraynev@mirantis.com>
2016-07-21 16:15:03 +0300

    Add test for packages structure
    Change-Id: I1f9cd14f41edc9f825d86c4867426a9805af2882

--- unittests/test_package_structure.py
+++ unittests/test_package_structure.py
@@ -5,8 +5,7 @@ import yaml
 import base
 
 
-class TestNamespaces(base.UnittetsBaseCiCd):
-
+class TestPackage(base.UnittetsBaseCiCd):
     def get_list_of_classes(self):
         class_names = []
 
@@ -17,47 +16,20 @@ class TestNamespaces(base.UnittetsBaseCiCd):
                 class_names.extend(names)
         return class_names
 
-    def get_namespaces(self, cls_name):
-        # workaround for PyYAML bug: http://pyyaml.org/ticket/221
-        ###############################
-        class YaqlYamlLoader(yaml.Loader):
-                pass
-
-        def yaql_constructor(loader, node):
-            return
-
-        YaqlYamlLoader.add_constructor(u'!yaql', yaql_constructor)
-        ###############################
-
-        # parse file
-        parsed_data = ''
-        with open(cls_name) as f:
-            parsed_data = yaml.load(f, YaqlYamlLoader)
-
-        n_spaces = parsed_data.get('Namespaces')
-        if n_spaces is None:
-            msg = 'File "%s" does not content "Namespaces" section' % cls_name
-            raise ValueError(msg)
-        # get only names of namespaces
-        names = n_spaces.keys()
-        # remove main namespace from the list
-        names.remove('=')
-        return names
-
     def check_name(self, namespace, cls_name, error_list):
         # read file
         data = ''
         with open(cls_name) as f:
             data = f.read()
 
-        regexp_str = '[^a-zA-Z]%s:[a-zA-Z]+' % namespace
+        regexp_str = '%s:[A-Z]+' % namespace
         regexp = re.compile(regexp_str)
         if len(regexp.findall(data)) == 0:
             msg = ('Namespace "%s" is not used in the "%s" and should '
                    'be removed from list of Namespaces' % (namespace, cls_name))
             error_list.append(msg)
 
-    def test_namespaces(self):
+    def test_package_structure(self):
         error_list = []
         for cls_name in self.get_list_of_classes():
             for ns in self.get_namespaces(cls_name):
