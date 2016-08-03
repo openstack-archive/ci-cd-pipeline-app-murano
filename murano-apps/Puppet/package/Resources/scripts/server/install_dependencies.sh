bdb1f00efbf4b30f7eed26579ab42fa3222d0565
Sergey Kraynev <skraynev@mirantis.com>
2016-08-03 12:59:16 +0300

    Fix errors reported by shellcheck for Puppet
    Change-Id: I8277c6265501c74d07493f723472fbf4b85ca29e

--- murano-apps/Puppet/package/Resources/scripts/server/install_dependencies.sh
+++ murano-apps/Puppet/package/Resources/scripts/server/install_dependencies.sh
@@ -5,7 +5,7 @@ ENV_NAME="$1"
 PUPPETFILE_LINK="$2"
 ENV_DIR="/etc/puppet/environments/${ENV_NAME}"
 
-cd ${ENV_DIR}
-wget --quiet -c ${PUPPETFILE_LINK} -O Puppetfile
+cd "${ENV_DIR}"
+wget --quiet -c "${PUPPETFILE_LINK}" -O Puppetfile
 
 librarian-puppet install
