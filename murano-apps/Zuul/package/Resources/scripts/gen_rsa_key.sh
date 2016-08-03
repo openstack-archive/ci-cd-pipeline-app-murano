7d81d4dd275c6bd78cb0cc56353fc8856a638a50
Sergey Kraynev <skraynev@mirantis.com>
2016-08-03 12:38:06 +0300

    Fix errors reported by shellcheck for Zuul and Nodepool
    Change-Id: I2c5fbf234a1bb1c6c27a562f6b4aa6a7e2651ae2

--- murano-apps/Zuul/package/Resources/scripts/gen_rsa_key.sh
+++ murano-apps/Zuul/package/Resources/scripts/gen_rsa_key.sh
@@ -4,8 +4,8 @@ datafile='/etc/puppet/hieradata/murano.yaml'
 
 ssh-keygen -t rsa -P '' -f zuul_ssh_key -q
 
-content=`cat zuul_ssh_key`
+content=$(cat zuul_ssh_key)
 puppet apply --execute "yaml_setting { 'example': target=>'$datafile', key=>'zuul_ssh_private_key_contents', value=>'$content', }"
 
-content=`cat zuul_ssh_key.pub`
+content=$(cat zuul_ssh_key.pub)
 puppet apply --execute "yaml_setting { 'example': target=>'$datafile', key=>'zuul_ssh_pubkey_contents', value=>'$content', }"
