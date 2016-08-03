a17ba2b592613ce7808c2a0bc6a6c914334df78b
Sergey Kraynev <skraynev@mirantis.com>
2016-08-03 12:38:06 +0300

    Fix errors reported by shellcheck for Zuul and Nodepool
    Change-Id: I2c5fbf234a1bb1c6c27a562f6b4aa6a7e2651ae2

--- murano-apps/Nodepool/package/Resources/scripts/release_resources.sh
+++ murano-apps/Nodepool/package/Resources/scripts/release_resources.sh
@@ -9,20 +9,20 @@ server_ids=$(mysql -u nodepool --password=nodepool nodepool -e 'select external_
 image_ids=$(mysql -u nodepool --password=nodepool nodepool -e 'select external_id from snapshot_image' | grep -o '[-0-9a-f]\{36\}')
 
 # Get token and service catalog.
-resp=$(curl -H "Content-Type: application/json" -X POST $OS_AUTH_URL/tokens --data '{"auth": {"tenantName": "'$OS_TENANT_NAME'", "passwordCredentials": {"username": "'$OS_USERNAME'", "password": "'$OS_PASSWORD'"}}}')
+resp=$(curl -H "Content-Type: application/json" -X POST "$OS_AUTH_URL/tokens" --data '{"auth": {"tenantName": '"$OS_TENANT_NAME"', "passwordCredentials": {"username": '"$OS_USERNAME"', "password": '"$OS_PASSWORD"'}}}')
 
-token=$(echo $resp | json_pp | grep -A15 '"token"' | sed -e '/"tenant"/,+7d' | grep '"id"' | cut -d '"' -f 4)
-nova_url=$(echo $resp | json_pp | grep '8774/v2' | grep publicURL | cut -d '"' -f 4)
-glance_url=$(echo $resp | json_pp | grep ':9292' | grep publicURL | cut -d '"' -f 4)
+token=$(echo "$resp" | json_pp | grep -A15 '"token"' | sed -e '/"tenant"/,+7d' | grep '"id"' | cut -d '"' -f 4)
+nova_url=$(echo "$resp" | json_pp | grep '8774/v2' | grep publicURL | cut -d '"' -f 4)
+glance_url=$(echo "$resp" | json_pp | grep ':9292' | grep publicURL | cut -d '"' -f 4)
 
 # Clean up servers.
 for id in $server_ids
 do
-  curl -X DELETE -H "X-Auth-Token: $token" $nova_url/servers/$id
+  curl -X DELETE -H "X-Auth-Token: $token" "$nova_url/servers/$id"
 done
 
 # Clean up images.
 for id in $image_ids
 do
-  curl -X DELETE -H "X-Auth-Token: $token" $glance_url/v1/images/$id
+  curl -X DELETE -H "X-Auth-Token: $token" "$glance_url/v1/images/$id"
 done
