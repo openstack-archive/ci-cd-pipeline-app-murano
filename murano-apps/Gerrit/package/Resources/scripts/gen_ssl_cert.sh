805c2cef0e3c91ffdd4892d7a3408a9cc7959ea9
Sergey Kraynev <skraynev@mirantis.com>
2016-08-03 12:24:13 +0300

    Fix errors reported by shellcheck for Gerrit and Jenkins
    Change-Id: Ia383b6e788429fc3170d0362afded8f3bc43e61f

--- murano-apps/Gerrit/package/Resources/scripts/gen_ssl_cert.sh
+++ murano-apps/Gerrit/package/Resources/scripts/gen_ssl_cert.sh
@@ -3,7 +3,7 @@ logger Generate SSL certificate for Gerrit
 
 datafile='/etc/puppet/hieradata/murano.yaml'
 
-fqdn=`hostname`
+fqdn=$(hostname)
 
 openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=$fqdn" \
     -keyout server.key -out server.cert
@@ -11,10 +11,10 @@ openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=Denial/
 cp server.key server.key.org
 openssl rsa -in server.key.org -out server.key
 
-content=`cat server.key`
+content=$(cat server.key)
 puppet apply --execute "yaml_setting { 'example': target=>'$datafile', key=>'gerrit_ssl_key_file_contents', value=>'$content', }"
 
-content=`cat server.cert`
+content=$(cat server.cert)
 puppet apply --execute "yaml_setting { 'example': target=>'$datafile', key=>'gerrit_ssl_cert_file_contents', value=>'$content', }"
 
 content=''
