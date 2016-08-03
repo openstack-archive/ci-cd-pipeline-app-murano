75b66f4428f4a30bcef36300ca7f8f977c6bd2f8
Sergey Kraynev <skraynev@mirantis.com>
2016-08-03 12:30:12 +0300

    Fix errors reported by shellcheck for SystemConfig and OpenLDAP
    Change-Id: I95dd70789c28d6c9727c75ca29ee618a72ba4e27

--- murano-apps/SystemConfig/package/Resources/scripts/deploy.sh
+++ murano-apps/SystemConfig/package/Resources/scripts/deploy.sh
@@ -9,24 +9,24 @@ logger Cloning openstack-ci system-config
 repository_name="$(basename "$URL")"
 
 if [ -n "$PATCH_ID" ] ; then
-   git clone $URL
-   pushd $repository_name
-       patch_ref=`git ls-remote | grep $PATCH_ID | tail -1 | awk '{print $2}'`
+   git clone "$URL"
+   pushd "$repository_name"
+       patch_ref=$(git ls-remote | grep "$PATCH_ID" | tail -1 | awk '{print $2}')
        if [ -z "$patch_ref" ] ; then
            echo "Patch id $PATCH_ID is not correct."
            exit 1
        fi
-       git fetch $URL $patch_ref && git checkout "FETCH_HEAD"
+       git fetch "$URL" "$patch_ref" && git checkout "FETCH_HEAD"
    popd
 elif [ -n "$BRANCH" ] ; then
-  git clone $URL --branch $BRANCH
+  git clone "$URL" --branch "$BRANCH"
 else
-  git clone $URL
+  git clone "$URL"
 fi
 
 logger Installing openstack-ci system-config
 
-pushd $repository_name
+pushd "$repository_name"
     ./install_modules.sh
 
     cp -r modules/ /etc/puppet/
